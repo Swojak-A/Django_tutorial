fcfd1dae9a891d78de0475a52a7265fd3f4a4b62
kaoss <swojak.a@gmail.com>
2019-06-11 22:52:48 +0200

    feat: add basic routes for polls

--- tutorial_app/polls/views.py
+++ tutorial_app/polls/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You are at the polls index.")
+    response = "Hello, world. You are at the polls index."
+    return HttpResponse(response)
+
+def detail(request, question_id):
+    response = f"You're looking at question {question_id}"
+    return HttpResponse(response)
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    response = f"You're voting on question {question_id}"
+    return HttpResponse(response)
