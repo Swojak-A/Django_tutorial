ab547b681fdf6ad84ec0c594e7c5d38783701e44
kaoss <swojak.a@gmail.com>
2019-06-12 18:53:23 +0200

    chore: refactor http request and template into render method

--- tutorial_app/polls/views.py
+++ tutorial_app/polls/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
@@ -7,12 +6,11 @@ from .models import Question
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
 
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_questions,
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     response = f"You're looking at question {question_id}"
